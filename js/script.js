c8a57497c0dca2a8d45269d005e02cba435c7aec
YoussefAlbatal05 <165221337+YoussefAlbatal05@users.noreply.github.com>
2024-09-27 17:34:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+var btn1 = document.getElementById("button1")
+
+function code1(){
+    alert("Copied!")
+}
+
+btn1.onclick = code1
+
+
+
+
+var btn1 = document.getElementById("button2")
+
+function code1(){
+    alert("Copied!")
+}
+
+btn1.onclick = code1
+
+
+
+
+var btn1 = document.getElementById("button3")
+
+function code1(){
+    alert("Copied!")
+}
+
+btn1.onclick = code1
+
